50e1c6c0e1d3b3766aa62e830739b2fd37445f4e
chrisjal <cjalallian@gmail.com>
2017-08-21 18:12:02 -0700

    Added basic files and package.json w/dependencies

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'DRwFV2dhZhYE736jnhcnVM4vB',
+  consumer_secret: 'LFWKYkf5yzy02hvQepLgv5TKgn0SmPZ4k2aktxyXvaSnLtTsqP',
+  access_token_key: '21994817-mBRffPWrenzorJZRzld0rpXHGLywO2rMUeY0AJelZ',
+  access_token_secret: 'JxEhryAiRqQpWFr8rkYUkymcxRF3qX2kSAZqKp0OWn0eJ',
+}
